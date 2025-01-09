57df05d22b4853e4b58aad6d2717499adb2e326c
beom84 <cjja00@konkuk.ac.kr>
2025-01-09 23:59:22 +0900

    [feat] #5 color 상수화/lintCheck

--- app/src/main/java/org/android/bbangzip/ui/theme/Color.kt
+++ app/src/main/java/org/android/bbangzip/ui/theme/Color.kt
@@ -2,110 +2,57 @@ package org.android.bbangzip.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-// val Common100: Color = Color(0xFFFFFFFF)
-// val Common0: Color = Color(0xFF000000)
-//
-// //Neutral
-// val Neutral99 = Color(0xFFF5F5F5)
-// val Neutral95 = Color(0xFFE9E9E8)
-// val Neutral90 = Color(0xFFD4D3D1)
-// val Neutral85 = Color(0xFFBEBCBA)
-// val Neutral80 = Color(0xFFA9A6A3)
-// val Neutral75 = Color(0xFF93908C)
-// val Neutral70 = Color(0xFF7E7A75)
-// val Neutral60 = Color(0xFF68645E)
-// val Neutral50 = Color(0xFF534D47)
-// val Neutral40 = Color(0xFF3D3730)
-// val Neutral30 = Color(0xFF282119)
-// val Neutral20 = Color(0xFF211B15)
-// val Neutral15 = Color(0xFF1B1611)
-// val Neutral10 = Color(0xFF14100C)
-// val Neutral7 = Color(0xFF0D0B08)
-// val Neutral5 = Color(0xFF080705)
-//
-// //Yellow
-// val Yellow99 = Color(0xFFFFF9EF)
-// val Yellow95 = Color(0xFFFFF3DF)
-// val Yellow92 = Color(0xFFFFECCF)
-// val Yellow90 = Color(0xFFFFE6C0)
-// val Yellow85 = Color(0xFFFFE0B0)
-// val Yellow80 = Color(0xFFFFDAA0)
-// val Yellow75 = Color(0xFFFFD490)
-// val Yellow70 = Color(0xFFFFCD80)
-// val Yellow65 = Color(0xFFFFC771)
-// val Yellow60 = Color(0xFFFFC161)
-// val Yellow50 = Color(0xFFFFB84A)
-// val Yellow40 = Color(0xFFF8A92F)
-// val Yellow30 = Color(0xFFDC901B)
-// val Yellow20 = Color(0xFFC67807)
-// val Yellow15 = Color(0xFFB25C03)
-// val Yellow10 = Color(0xFF9D440B)
-// val Yellow7 = Color(0xFF823108)
-// val Yellow5 = Color(0xFF621F05)
-//
-// //Orange
-// val Orange95 = Color(0xFFFFE6DA)
-// val Orange90 = Color(0xFFFFCDB5)
-// val Orange80 = Color(0xFFFFB58F)
-// val Orange70 = Color(0xFFFF9C6A)
-// val Orange60 = Color(0xFFFF8345)
-// val Orange50 = Color(0xFFE5692B)
-// val Orange40 = Color(0xFFB23600)
-// val Orange30 = Color(0xFF991D00)
-// val Orange20 = Color(0xFF660000)
-// val Orange10 = Color(0xFF4C0000)
+// Common
+val Common100: Color = Color(0xFFFFFFFF)
+val Common0: Color = Color(0xFF000000)
 
-// Primary
-val PrimaryLight = Color(0xFF3D3730)
-val PrimaryNormal = Color(0xFF282119)
-val PrimaryHeavy = Color(0xFF1B1611)
+// Neutral
+val Neutral99 = Color(0xFFF5F5F5)
+val Neutral95 = Color(0xFFE9E9E8)
+val Neutral90 = Color(0xFFD4D3D1)
+val Neutral85 = Color(0xFFBEBCBA)
+val Neutral80 = Color(0xFFA9A6A3)
+val Neutral75 = Color(0xFF93908C)
+val Neutral70 = Color(0xFF7E7A75)
+val Neutral60 = Color(0xFF68645E)
+val Neutral50 = Color(0xFF534D47)
+val Neutral40 = Color(0xFF3D3730)
+val Neutral30 = Color(0xFF282119)
+val Neutral20 = Color(0xFF211B15)
+val Neutral15 = Color(0xFF1B1611)
+val Neutral10 = Color(0xFF14100C)
+val Neutral7 = Color(0xFF0D0B08)
+val Neutral5 = Color(0xFF080705)
 
-// Secondary
-val SecondaryLight = Color(0xFFFFCD80)
-val SecondaryNormal = Color(0xFFFFC161)
-val SecondaryHeavy = Color(0xFFF8A92F)
+// Yellow
+val Yellow99 = Color(0xFFFFF9EF)
+val Yellow95 = Color(0xFFFFF3DF)
+val Yellow92 = Color(0xFFFFECCF)
+val Yellow90 = Color(0xFFFFE6C0)
+val Yellow85 = Color(0xFFFFE0B0)
+val Yellow80 = Color(0xFFFFDAA0)
+val Yellow75 = Color(0xFFFFD490)
+val Yellow70 = Color(0xFFFFCD80)
+val Yellow65 = Color(0xFFFFC771)
+val Yellow60 = Color(0xFFFFC161)
+val Yellow55 = Color(0xFFFEE500) // kakaoContainer
+val Yellow50 = Color(0xFFFFB84A)
+val Yellow40 = Color(0xFFF8A92F)
+val Yellow30 = Color(0xFFDC901B)
+val Yellow20 = Color(0xFFC67807)
+val Yellow15 = Color(0xFFB25C03)
+val Yellow10 = Color(0xFF9D440B)
+val Yellow7 = Color(0xFF823108)
+val Yellow5 = Color(0xFF621F05)
 
-// Label
-val LabelStrong = Color(0xFF000000)
-val LabelNormal = Color(0xFF282119)
-val LabelNeutral = Color(0xFF282119).copy(alpha = 0.74f)
-val LabelAlternative = Color(0xFF282119).copy(alpha = 0.52f)
-val LabelAssistive = Color(0xFF282119).copy(alpha = 0.28f)
-val LabelDisable = Color(0xFF282119).copy(alpha = 0.12f)
-
-// Background
-val BackgroundNormal = Color(0xFFFFFFFF)
-val BackgroundAlternative = Color(0xFFF5F5F5)
-val BackgroundAccent = Color(0xFFFFDAA0)
-
-// Interaction
-val InteractionInactive = Color(0xFFD4D3D1)
-val InteractionDisable = Color(0xFFF5F5F5)
-
-// Line
-val LineNormal = Color(0xFF68645E).copy(alpha = 0.22f)
-val LineNeutral = Color(0xFF68645E).copy(alpha = 0.16f) // Adjusted with opacity from photo
-val LineAlternative = Color(0xFF68645E).copy(alpha = 0.08f)
-val LineStrong = Color(0xFF68645E).copy(alpha = 0.52f)
-
-// Status
-val StatusPositive = Color(0xFF3D3730)
-val StatusCautionary = Color(0xFFFFB84A)
-val StatusDestructive = Color(0xFFFF8345)
-
-// Static
-val StaticWhite = Color(0xFFFFFFFF)
-val StaticBlack = Color(0xFF000000)
-
-// Component Fill
-val FillNormal = Color(0xFF68645E).copy(alpha = 0.08f)
-val FillStrong = Color(0xFF68645E).copy(alpha = 0.16f)
-val FillAlternative = Color(0xFF68645E).copy(alpha = 0.05f)
-
-// Material
-val MaterialDimmer = Color(0xFF282119).copy(0.52f)
-
-// Social Kakao
-val KakaoContainer = Color(0xFFFEE500)
-val KakaoSymbol = Color(0xFF000000)
-val KakaoLabel = Color(0xFF000000).copy(alpha = 0.85f)
+// Orange
+val Orange95 = Color(0xFFFFE6DA)
+val Orange90 = Color(0xFFFFCDB5)
+val Orange80 = Color(0xFFFFB58F)
+val Orange70 = Color(0xFFFF9C6A)
+val Orange60 = Color(0xFFFF8345)
+val Orange50 = Color(0xFFE5692B)
+val Orange40 = Color(0xFFB23600)
+val Orange30 = Color(0xFF991D00)
+val Orange20 = Color(0xFF660000)
+val Orange10 = Color(0xFF4C0000)
--- app/src/main/java/org/android/bbangzip/ui/theme/ColorScheme.kt
+++ app/src/main/java/org/android/bbangzip/ui/theme/ColorScheme.kt
@@ -7,97 +7,97 @@ import androidx.compose.ui.graphics.Color
 @Immutable
 data class BbangZipColors(
     // Primary
-    val primaryLight: Color,
-    val primaryNormal: Color,
-    val primaryHeavy: Color,
+    val primaryLight_3D3730: Color,
+    val primaryNormal_282119: Color,
+    val primaryHeavy_1B1611: Color,
     // Secondary
-    val secondaryLight: Color,
-    val secondaryNormal: Color,
-    val secondaryHeavy: Color,
+    val secondaryLight_FFD480: Color,
+    val secondaryNormal_FFC161: Color,
+    val secondaryHeavy_FFB84A: Color,
     // Label
-    val labelStrong: Color,
-    val labelNormal: Color,
-    val labelNeutral: Color,
-    val labelAlternative: Color,
-    val labelAssistive: Color,
-    val labelDisable: Color,
+    val labelStrong_000000: Color,
+    val labelNormal_282119: Color,
+    val labelNeutral_282119_74: Color,
+    val labelAlternative_282119_52: Color,
+    val labelAssistive_282119_28: Color,
+    val labelDisable_282119_12: Color,
     // Background
-    val backgroundNormal: Color,
-    val backgroundAlternative: Color,
-    val backgroundAccent: Color,
+    val backgroundNormal_FFFFFF: Color,
+    val backgroundAlternative_F5F5F5: Color,
+    val backgroundAccent_FFDAA0: Color,
     // Interaction
-    val interactionInactive: Color,
-    val interactionDisable: Color,
+    val interactionInactive_D4D3D1: Color,
+    val interactionDisable_F5F5F5: Color,
     // Line
-    val lineNormal: Color,
-    val lineNeutral: Color,
-    val lineAlternative: Color,
-    val lineStrong: Color,
+    val lineNormal_68645E_22: Color,
+    val lineNeutral_68645E_16: Color,
+    val lineAlternative_68645E_08: Color,
+    val lineStrong_68645E_52: Color,
     // Status
-    val statusPositive: Color,
-    val statusCautionary: Color,
-    val statusDestructive: Color,
+    val statusPositive_3D3730: Color,
+    val statusCautionary_FFB84A: Color,
+    val statusDestructive_FF8345: Color,
     // Static
-    val staticWhite: Color,
-    val staticBlack: Color,
+    val staticWhite_FFFFFF: Color,
+    val staticBlack_000000: Color,
     // Component Fill
-    val fillNormal: Color,
-    val fillStrong: Color,
-    val fillAlternative: Color,
+    val fillNormal_68645E_08: Color,
+    val fillStrong_68645E_16: Color,
+    val fillAlternative_68645E_05: Color,
     // Material
-    val materialDimmer: Color,
+    val materialDimmer_282119_52: Color,
     // Social Kakao
-    val kakaoContainer: Color,
-    val kakaoSymbol: Color,
-    val kakaoLabel: Color,
+    val kakaoContainer_FEE500: Color,
+    val kakaoSymbol_000000: Color,
+    val kakaoLabel_000000_85: Color,
 )
 
 val defaultBbangZipColors =
     BbangZipColors(
         // Primary
-        primaryLight = PrimaryLight,
-        primaryNormal = PrimaryNormal,
-        primaryHeavy = PrimaryHeavy,
+        primaryLight_3D3730 = Neutral40,
+        primaryNormal_282119 = Neutral30,
+        primaryHeavy_1B1611 = Neutral15,
         // Secondary
-        secondaryLight = SecondaryLight,
-        secondaryNormal = SecondaryNormal,
-        secondaryHeavy = SecondaryHeavy,
+        secondaryLight_FFD480 = Yellow70,
+        secondaryNormal_FFC161 = Yellow60,
+        secondaryHeavy_FFB84A = Yellow50,
         // Label
-        labelStrong = LabelStrong,
-        labelNormal = LabelNormal,
-        labelNeutral = LabelNeutral,
-        labelAlternative = LabelAlternative,
-        labelAssistive = LabelAssistive,
-        labelDisable = LabelDisable,
+        labelStrong_000000 = Common0,
+        labelNormal_282119 = Neutral30,
+        labelNeutral_282119_74 = Neutral30.copy(alpha = 0.74f),
+        labelAlternative_282119_52 = Neutral30.copy(alpha = 0.52f),
+        labelAssistive_282119_28 = Neutral30.copy(alpha = 0.28f),
+        labelDisable_282119_12 = Neutral30.copy(alpha = 0.12f),
         // Background
-        backgroundNormal = BackgroundNormal,
-        backgroundAlternative = BackgroundAlternative,
-        backgroundAccent = BackgroundAccent,
+        backgroundNormal_FFFFFF = Common100,
+        backgroundAlternative_F5F5F5 = Neutral99,
+        backgroundAccent_FFDAA0 = Yellow80,
         // Interaction
-        interactionInactive = InteractionInactive,
-        interactionDisable = InteractionDisable,
+        interactionInactive_D4D3D1 = Neutral90,
+        interactionDisable_F5F5F5 = Neutral99,
         // Line
-        lineNormal = LineNormal,
-        lineNeutral = LineNeutral,
-        lineAlternative = LineAlternative,
-        lineStrong = LineStrong,
+        lineNormal_68645E_22 = Neutral60.copy(alpha = 0.22f),
+        lineNeutral_68645E_16 = Neutral60.copy(alpha = 0.16f),
+        lineAlternative_68645E_08 = Neutral60.copy(alpha = 0.08f),
+        lineStrong_68645E_52 = Neutral60.copy(alpha = 0.52f),
         // Status
-        statusPositive = StatusPositive,
-        statusCautionary = StatusCautionary,
-        statusDestructive = StatusDestructive,
+        statusPositive_3D3730 = Neutral40,
+        statusCautionary_FFB84A = Yellow50,
+        statusDestructive_FF8345 = Orange60,
         // Static
-        staticWhite = StaticWhite,
-        staticBlack = StaticBlack,
+        staticWhite_FFFFFF = Common100,
+        staticBlack_000000 = Common0,
         // Component Fill
-        fillNormal = FillNormal,
-        fillStrong = FillStrong,
-        fillAlternative = FillAlternative,
+        fillNormal_68645E_08 = Neutral60.copy(alpha = 0.08f),
+        fillStrong_68645E_16 = Neutral60.copy(alpha = 0.16f),
+        fillAlternative_68645E_05 = Neutral60.copy(alpha = 0.05f),
         // Material
-        materialDimmer = MaterialDimmer,
+        materialDimmer_282119_52 = Neutral30.copy(alpha = 0.52f),
         // Social Kakao
-        kakaoContainer = KakaoContainer,
-        kakaoSymbol = KakaoSymbol,
-        kakaoLabel = KakaoLabel,
+        kakaoContainer_FEE500 = Yellow55,
+        kakaoSymbol_000000 = Common0,
+        kakaoLabel_000000_85 = Common0.copy(alpha = 0.85f),
     )
 
 val LocalBbangZipColors = staticCompositionLocalOf { defaultBbangZipColors }
