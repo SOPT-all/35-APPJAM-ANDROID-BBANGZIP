cacf49add8966e52b0c3fc2a0b11f37e97e3adf7
JIEUNI <jieundaeun@naver.com>
2025-01-12 05:50:33 +0900

    [feat] #9 BbangZipBasicTextFieldSlot 구현

---
+++ app/src/main/java/org/android/bbangzip/presentation/component/textfield/BbangZipBasicTextFieldSlot.kt
@@ -0,0 +1,28 @@
+package org.android.bbangzip.presentation.component.textfield
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun BbangZipBasicTextFieldSlot(
+    modifier: Modifier = Modifier,
+    leadingIcon: @Composable (RowScope.() -> Unit) = {},
+    content: @Composable (RowScope.() -> Unit) = {},
+    characterCount: @Composable (RowScope.() -> Unit) = {},
+    trailingIcon: @Composable (RowScope.() -> Unit) = {}
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        leadingIcon()
+        content()
+        characterCount()
+        trailingIcon()
+    }
+}
