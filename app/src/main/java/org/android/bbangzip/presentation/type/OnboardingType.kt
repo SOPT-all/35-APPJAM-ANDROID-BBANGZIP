c49b05592b9631060619f4258a2d2b77d7d1c85a
JIEUNI <jieundaeun@naver.com>
2025-01-14 03:57:11 +0900

    [feat] #18 OnboardingType 구현

---
+++ app/src/main/java/org/android/bbangzip/presentation/type/OnboardingType.kt
@@ -0,0 +1,21 @@
+package org.android.bbangzip.presentation.type
+
+import androidx.annotation.StringRes
+import org.android.bbangzip.R
+
+enum class OnboardingType(
+    @StringRes val title: Int,
+    @StringRes val description: Int? = null
+) {
+    FIRST(
+        title = R.string.onboarding_first_title
+    ),
+    SECOND(
+        title = R.string.onboarding_second_title,
+        description = R.string.onboarding_second_description
+    ),
+    THIRD(
+        title = R.string.onboarding_third_title,
+        description = R.string.onboarding_third_description
+    )
+}
