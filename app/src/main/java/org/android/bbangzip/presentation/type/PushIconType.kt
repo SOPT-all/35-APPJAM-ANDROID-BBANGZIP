19af4730716008139824ef87dbde06d212446646
JIEUNI <jieundaeun@naver.com>
2025-01-13 16:51:53 +0900

    [feat] #18 PushIconType 구현

---
+++ app/src/main/java/org/android/bbangzip/presentation/type/PushIconType.kt
@@ -0,0 +1,19 @@
+package org.android.bbangzip.presentation.type
+
+
+import androidx.compose.ui.graphics.Color
+import org.android.bbangzip.ui.theme.defaultBbangZipColors
+
+enum class PushIconType(
+    val backgroundColor: Color,
+    val textColor: Color
+) {
+    POSITIVE(
+        backgroundColor = defaultBbangZipColors.statusPositive_3D3730,
+        textColor = defaultBbangZipColors.staticWhite_FFFFFF
+    ),
+    DISABLE(
+        backgroundColor = defaultBbangZipColors.fillAlternative_68645E_05,
+        textColor = defaultBbangZipColors.labelDisable_282119_12
+    )
+}
