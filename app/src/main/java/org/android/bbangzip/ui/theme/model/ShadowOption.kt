f027333433b75cdc1cf1bf9d642cec23b2d82790
beom84 <cjja00@konkuk.ac.kr>
2025-01-14 16:18:31 +0900

    [feat] #21 shadowConfig -> ShadowOption 이름 변경

--- app/src/main/java/org/android/bbangzip/ui/theme/model/ShadowOption.kt
+++ app/src/main/java/org/android/bbangzip/ui/theme/model/ShadowOption.kt
@@ -3,7 +3,7 @@ package org.android.bbangzip.ui.theme.model
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 
-data class ShadowConfig(
+data class ShadowOption(
     val color: Color,
     val blur: Dp,
     val offsetX: Dp,
