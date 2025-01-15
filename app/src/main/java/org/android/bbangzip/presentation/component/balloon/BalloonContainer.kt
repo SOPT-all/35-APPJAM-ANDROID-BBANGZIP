6752c5373c661fc8476aff60d6d1957c0b97a6da
beom84 <cjja00@konkuk.ac.kr>
2025-01-15 22:22:53 +0900

    [feat] #26 ballonContainer 구현

---
+++ app/src/main/java/org/android/bbangzip/presentation/component/balloon/BalloonContainer.kt
@@ -0,0 +1,52 @@
+package org.android.bbangzip.presentation.component.balloon
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import org.android.bbangzip.ui.theme.BbangZipTheme
+
+@Composable
+fun BalloonContainer(
+    text: String,
+    leadingIcon: @Composable () -> Unit = {},
+    trailingIcon: @Composable () -> Unit = {},
+    modifier: Modifier = Modifier,
+) {
+    Box(
+        modifier =
+            modifier
+                .fillMaxWidth()
+                .background(
+                    color = BbangZipTheme.colors.staticWhite_FFFFFF,
+                    shape = RoundedCornerShape(size = 20.dp),
+                )
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+        Row {
+            leadingIcon()
+
+            Spacer(modifier = Modifier.width(6.dp))
+
+            Text(
+                text = text,
+                style = BbangZipTheme.typography.body1Bold,
+                color = BbangZipTheme.colors.labelNormal_282119,
+            )
+
+            Spacer(modifier = Modifier.width(6.dp))
+
+            trailingIcon()
+        }
+    }
+}
