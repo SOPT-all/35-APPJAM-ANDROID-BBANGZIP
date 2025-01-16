13c186a37dec54173db057175d25a55e40d34f4d
kamja0510 <kamja990510@gmail.com>
2025-01-17 05:19:30 +0900

    [chore] #33 ktlint 체크

--- app/src/main/java/org/android/bbangzip/presentation/component/topbar/BbangZipTopBarSlot.kt
+++ app/src/main/java/org/android/bbangzip/presentation/component/topbar/BbangZipTopBarSlot.kt
@@ -3,11 +3,9 @@ package org.android.bbangzip.presentation.component.topbar
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun BbangZipTopBarSlot(
