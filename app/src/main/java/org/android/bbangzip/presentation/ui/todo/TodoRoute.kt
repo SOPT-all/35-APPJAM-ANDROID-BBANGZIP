341389608e86c11e5ef503faecae67bd566c1478
beom84 <cjja00@konkuk.ac.kr>
2025-01-17 19:09:11 +0900

    [feat] #31 Route 함수 구현

--- app/src/main/java/org/android/bbangzip/presentation/ui/todo/TodoRoute.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/todo/TodoRoute.kt
@@ -1,8 +1,84 @@
 package org.android.bbangzip.presentation.ui.todo
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
-fun TodoRoute() {
-    TodoScreen()
+fun TodoRoute(
+    snackBarHostState: SnackbarHostState,
+    navigateToAddToDo: () -> Unit = {},
+    navigateToAddPendingToDo: () -> Unit = {},
+    viewModel: TodoViewModel = hiltViewModel()
+) {
+    val todoState by viewModel.uiState.collectAsStateWithLifecycle()
+    val success by viewModel.success.collectAsStateWithLifecycle(initialValue = false)
+
+    LaunchedEffect(viewModel.uiSideEffect) {
+        viewModel.uiSideEffect.collectLatest { effect ->
+            when (effect) {
+                TodoContract.TodoSideEffect.NavigateToAddPendingToDo ->
+                    navigateToAddPendingToDo()
+
+                TodoContract.TodoSideEffect.NavigateToAddToDo ->
+                    navigateToAddToDo()
+
+                is TodoContract.TodoSideEffect.ShowSnackBar ->
+                    snackBarHostState.showSnackbar(effect.message)
+            }
+        }
+    }
+
+    when (success) {
+        true ->
+            TodoScreen(
+                todoState = todoState,
+                snackBarHostState = snackBarHostState,
+                onAddPendingStudyButtonClicked = {
+                    viewModel.setEvent(TodoContract.TodoEvent.OnAddPendingStudyButtonClicked)
+                },
+                onAddStudyButtonClicked = {
+                    viewModel.setEvent(TodoContract.TodoEvent.OnAddStudyButtonClicked)
+                },
+                onRevertCompleteBottomSheetDismissButtonClicked = {
+                    viewModel.setEvent(TodoContract.TodoEvent.OnRevertCompleteBottomSheetDismissButtonClicked)
+                },
+                onRevertCompleteBottomSheetApproveButtonClicked = { pieceId, cardState ->
+                    viewModel.setEvent(
+                        TodoContract.TodoEvent.OnRevertCompleteBottomSheetApproveButtonClicked(pieceId = pieceId, cardState = cardState)
+                    )
+                },
+                onRevertCompleteBottomSheetDismissRequest = {
+                    viewModel.setEvent(TodoContract.TodoEvent.OnFilterBottomSheetDismissRequest)
+                },
+                onFilterIconClicked = {
+                    viewModel.setEvent(TodoContract.TodoEvent.OnFilterIconClicked)
+                },
+                onFilterBottomSheetItemClicked = { filterIndex ->
+                    viewModel.setEvent(TodoContract.TodoEvent.OnFilterBottomSheetItemClicked(todoFilterItemIndex = filterIndex))
+                },
+                onFilterBottomSheetDismissRequest ={
+                    viewModel.setEvent(TodoContract.TodoEvent.OnFilterBottomSheetDismissRequest)
+                },
+                onDeleteIconClicked ={
+                    viewModel.setEvent(TodoContract.TodoEvent.OnDeleteIconClicked)
+                },
+                onCloseIconClicked = {
+                    viewModel.setEvent(TodoContract.TodoEvent.OnCloseIconClicked)
+                },
+                onItemDeleteButtonClicked ={
+                    viewModel.setEvent(TodoContract.TodoEvent.OnItemDeleteButtonClicked)
+                },
+                onDeleteScreenCardClicked = { pieceId ->
+                    viewModel.setEvent(TodoContract.TodoEvent.OnDeleteScreenCardClicked(pieceId = pieceId))
+                },
+            )
+
+        false ->
+            null
+    }
 }
--- app/src/main/java/org/android/bbangzip/presentation/ui/todo/TodoScreen.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/todo/TodoScreen.kt
@@ -1,12 +1,28 @@
 package org.android.bbangzip.presentation.ui.todo
 
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import org.android.bbangzip.presentation.component.card.BbangZipCardState
 
 @Composable
 fun TodoScreen(
+    todoState: TodoContract.TodoState,
+    snackBarHostState: SnackbarHostState,
     modifier: Modifier = Modifier,
+    onAddPendingStudyButtonClicked: () -> Unit = {},
+    onAddStudyButtonClicked: () -> Unit = {},
+    onRevertCompleteBottomSheetDismissButtonClicked: () -> Unit = {},
+    onRevertCompleteBottomSheetApproveButtonClicked: (Int, BbangZipCardState) -> Unit = { _, _ -> },
+    onRevertCompleteBottomSheetDismissRequest: () -> Unit = {},
+    onFilterIconClicked: () -> Unit = {},
+    onFilterBottomSheetItemClicked: (Int) -> Unit = {},
+    onFilterBottomSheetDismissRequest: () -> Unit = {},
+    onDeleteIconClicked: () -> Unit = {},
+    onCloseIconClicked: () -> Unit = {},
+    onItemDeleteButtonClicked: () -> Unit = {},
+    onDeleteScreenCardClicked: (Int) -> Unit = {}
 ) {
     Text("todo 탭")
 }
