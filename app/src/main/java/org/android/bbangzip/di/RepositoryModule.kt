dd46ccaf16752482858af060b7687e17844a68c7
JIEUNI <jieundaeun@naver.com>
2025-01-17 16:27:09 +0900

    [init] #36 Proto DataStore 구현

--- app/src/main/java/org/android/bbangzip/di/RepositoryModule.kt
+++ app/src/main/java/org/android/bbangzip/di/RepositoryModule.kt
@@ -5,7 +5,9 @@ import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.android.bbangzip.data.repositoryImpl.DummyRepositoryImpl
-import org.android.bbangzip.domain.repository.DummyRepository
+import org.android.bbangzip.data.repositoryImpl.UserRepositoryImpl
+import org.android.bbangzip.domain.repository.local.UserRepository
+import org.android.bbangzip.domain.repository.remote.DummyRepository
 import javax.inject.Singleton
 
 @Module
@@ -14,4 +16,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindsDummyRepository(repositoryImpl: DummyRepositoryImpl): DummyRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindsUserRepository(repositoryImpl: UserRepositoryImpl): UserRepository
 }
---
+++ app/src/main/java/org/android/bbangzip/domain/repository/local/UserRepository.kt
@@ -0,0 +1,10 @@
+package org.android.bbangzip.domain.repository.local
+
+import kotlinx.coroutines.flow.Flow
+import org.android.bbangzip.UserPreferences
+
+interface UserRepository {
+    val userPreferenceFlow: Flow<UserPreferences>
+    suspend fun setUserData(accessToken: String)
+    suspend fun clearUserData()
+}
