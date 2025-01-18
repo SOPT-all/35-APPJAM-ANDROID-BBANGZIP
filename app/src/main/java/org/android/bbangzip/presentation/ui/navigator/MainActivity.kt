49499e6f2a82c39c93377fcba640cb6f006d70d4
JIEUNI <jieundaeun@naver.com>
2025-01-18 13:12:38 +0900

    [feat] #39 splash screen 연결

--- app/src/main/java/org/android/bbangzip/presentation/ui/navigator/MainActivity.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/navigator/MainActivity.kt
@@ -4,20 +4,41 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.delay
+import org.android.bbangzip.presentation.ui.splash.SplashScreen
 import org.android.bbangzip.ui.theme.BBANGZIPTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        installSplashScreen().setOnExitAnimationListener{ splashScreenView ->
+            splashScreenView.remove()
+        }
+
         enableEdgeToEdge()
         setContent {
             val navigator: MainNavigator = rememberMainNavigator()
+            var showSplash by remember { mutableStateOf(true) }
 
             BBANGZIPTheme {
-                MainScreen(navigator = navigator)
+                LaunchedEffect(Unit) {
+                    delay(2000L)
+                    showSplash = false
+                }
+                if (showSplash) {
+                    SplashScreen()
+                } else {
+                    MainScreen(navigator = navigator)
+                }
             }
         }
     }
---
+++ app/src/main/java/org/android/bbangzip/presentation/ui/splash/SplashScreen.kt
@@ -0,0 +1,39 @@
+package org.android.bbangzip.presentation.ui.splash
+
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import org.android.bbangzip.R
+import org.android.bbangzip.ui.theme.BBANGZIPTheme
+import org.android.bbangzip.ui.theme.BbangZipTheme
+
+@Composable
+fun SplashScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BbangZipTheme.colors.backgroundAccent_FFDAA0),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            modifier = Modifier.weight(1f),
+            painter = painterResource(id = R.drawable.img_login1),
+            contentDescription = null,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun SplashScreenPreview() {
+    BBANGZIPTheme {
+        SplashScreen()
+    }
+}
