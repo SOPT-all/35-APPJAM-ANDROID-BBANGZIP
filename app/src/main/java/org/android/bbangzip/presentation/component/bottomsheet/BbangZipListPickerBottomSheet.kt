e95a65227f8e2cc796a0a09c9d6204570a5dbf80
JIEUNI <jieundaeun@naver.com>
2025-01-18 22:39:11 +0900

    [chore] #16 ktlint 체크

--- app/src/main/java/org/android/bbangzip/presentation/component/bottomsheet/BbangZipListPickerBottomSheet.kt
+++ app/src/main/java/org/android/bbangzip/presentation/component/bottomsheet/BbangZipListPickerBottomSheet.kt
@@ -1,6 +1,5 @@
 package org.android.bbangzip.presentation.component.bottomsheet
 
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -8,9 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SheetValue
 import androidx.compose.material3.Text
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
