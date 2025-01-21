2e258d883fb57a36a70fe9cd6185871013e198bd
beom84 <cjja00@konkuk.ac.kr>
2025-01-22 03:10:55 +0900

    [feat] #51 MyBadgeCategory contract viewmodel 구현

---
+++ app/src/main/java/org/android/bbangzip/presentation/ui/my/mybadgecategory/MyBadgeCategoryContract.kt
@@ -0,0 +1,41 @@
+package org.android.bbangzip.presentation.ui.my.mybadgecategory
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.android.bbangzip.presentation.model.BadgeCategory
+import org.android.bbangzip.presentation.model.BadgeDetail
+import org.android.bbangzip.presentation.ui.todo.TodoContract
+import org.android.bbangzip.presentation.util.base.BaseContract
+
+class MyBadgeCategoryContract {
+    @Parcelize
+    data class MyBadgeCategoryState(
+        val badgeCategoryList1: List<BadgeCategory> = listOf(),
+        val badgeCategoryList2: List<BadgeCategory> = listOf(),
+        val badgeDetailBottomSheetState: Boolean = false,
+        val badgeDetail: BadgeDetail = BadgeDetail()
+    ) : BaseContract.State, Parcelable {
+        override fun toParcelable(): Parcelable = this
+    }
+
+    sealed interface MyBadgeCategoryEvent : BaseContract.Event {
+        data object Initialize : MyBadgeCategoryEvent
+        data object OnBackIconClicked : MyBadgeCategoryEvent
+        data object OnBadgeDetailBottomSheetDismissButtonClicked : MyBadgeCategoryEvent
+        data object OnBadgeDetailBottomSheetDismissRequest : MyBadgeCategoryEvent
+        data class OnBadgeCardClicked(val badgeName: String) : MyBadgeCategoryEvent
+    }
+
+    sealed interface MyBadgeCategoryReduce : BaseContract.Reduce {
+        data class UpdateBadgeCategoryList(
+            val badgeCategoryList1: List<BadgeCategory>, val badgeCategoryList2: List<BadgeCategory>
+        ) : MyBadgeCategoryReduce
+
+        data class UpdateBadgeDetailBottomSheetState(val badgeDetailBottomSheetState: Boolean) : MyBadgeCategoryReduce
+        data class UpdateBadgeDetail(val badgeDetail: BadgeDetail) : MyBadgeCategoryReduce
+    }
+
+    sealed interface MyBadgeCategorySideEffect : BaseContract.SideEffect {
+        data object NavigateToBack : MyBadgeCategorySideEffect
+    }
+}
---
+++ app/src/main/java/org/android/bbangzip/presentation/ui/my/mybadgecategory/MyBadgeCategoryViewModel.kt
@@ -0,0 +1,78 @@
+package org.android.bbangzip.presentation.ui.my.mybadgecategory
+
+import android.os.Parcelable
+import androidx.lifecycle.SavedStateHandle
+import dagger.hilt.android.lifecycle.HiltViewModel
+import org.android.bbangzip.presentation.util.base.BaseViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MyBadgeCategoryViewModel
+@Inject
+constructor(
+    savedStateHandle: SavedStateHandle,
+) : BaseViewModel<MyBadgeCategoryContract.MyBadgeCategoryEvent, MyBadgeCategoryContract.MyBadgeCategoryState, MyBadgeCategoryContract.MyBadgeCategoryReduce, MyBadgeCategoryContract.MyBadgeCategorySideEffect>(
+    savedStateHandle = savedStateHandle,
+) {
+    override fun createInitialState(savedState: Parcelable?): MyBadgeCategoryContract.MyBadgeCategoryState {
+        return savedState as? MyBadgeCategoryContract.MyBadgeCategoryState
+            ?: MyBadgeCategoryContract.MyBadgeCategoryState()
+    }
+
+    init {
+        setEvent(MyBadgeCategoryContract.MyBadgeCategoryEvent.Initialize)
+    }
+
+    override fun handleEvent(event: MyBadgeCategoryContract.MyBadgeCategoryEvent) {
+        when (event) {
+            MyBadgeCategoryContract.MyBadgeCategoryEvent.Initialize -> launch { initData() }
+            MyBadgeCategoryContract.MyBadgeCategoryEvent.OnBackIconClicked ->
+                setSideEffect(MyBadgeCategoryContract.MyBadgeCategorySideEffect.NavigateToBack)
+
+            is MyBadgeCategoryContract.MyBadgeCategoryEvent.OnBadgeCardClicked -> {
+                //서버에서 get하거 bottomsheet 띄우기
+                updateState(
+                    MyBadgeCategoryContract.MyBadgeCategoryReduce.UpdateBadgeDetailBottomSheetState(
+                        badgeDetailBottomSheetState = true
+                    )
+                )
+            }
+
+            MyBadgeCategoryContract.MyBadgeCategoryEvent.OnBadgeDetailBottomSheetDismissButtonClicked ->
+                updateState(
+                    MyBadgeCategoryContract.MyBadgeCategoryReduce.UpdateBadgeDetailBottomSheetState(
+                        badgeDetailBottomSheetState = false
+                    )
+                )
+            MyBadgeCategoryContract.MyBadgeCategoryEvent.OnBadgeDetailBottomSheetDismissRequest ->  updateState(
+                MyBadgeCategoryContract.MyBadgeCategoryReduce.UpdateBadgeDetailBottomSheetState(
+                    badgeDetailBottomSheetState = false
+                )
+            )
+        }
+    }
+
+    override fun reduceState(
+        state: MyBadgeCategoryContract.MyBadgeCategoryState,
+        reduce: MyBadgeCategoryContract.MyBadgeCategoryReduce
+    ): MyBadgeCategoryContract.MyBadgeCategoryState {
+       return when (reduce) {
+            is MyBadgeCategoryContract.MyBadgeCategoryReduce.UpdateBadgeCategoryList -> state.copy(
+                badgeCategoryList1 = reduce.badgeCategoryList1,
+                badgeCategoryList2 = reduce.badgeCategoryList2
+            )
+
+            is MyBadgeCategoryContract.MyBadgeCategoryReduce.UpdateBadgeDetail -> state.copy(
+                badgeDetail = reduce.badgeDetail
+            )
+
+            is MyBadgeCategoryContract.MyBadgeCategoryReduce.UpdateBadgeDetailBottomSheetState -> state.copy(
+                badgeDetailBottomSheetState = reduce.badgeDetailBottomSheetState
+            )
+        }
+    }
+
+    private fun initData() {
+
+    }
+}
