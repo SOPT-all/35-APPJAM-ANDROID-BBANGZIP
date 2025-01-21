789adf87b503f97cb6f8fcb3fdaabf36acca43a3
JIEUNI <jieundaeun@naver.com>
2025-01-21 18:22:59 +0900

    [feat] #49 온보딩 완료 API 구현

--- app/src/main/java/org/android/bbangzip/data/interceptor/AuthInterceptor.kt
+++ app/src/main/java/org/android/bbangzip/data/interceptor/AuthInterceptor.kt
@@ -1,28 +1,55 @@
 package org.android.bbangzip.data.interceptor
 
+import android.app.Application
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.Json
 import okhttp3.Interceptor
 import okhttp3.Response
+import org.android.bbangzip.domain.repository.local.UserLocalRepository
+import timber.log.Timber
 import javax.inject.Inject
 
 class AuthInterceptor
-    @Inject
-    constructor() : Interceptor {
-        override fun intercept(chain: Interceptor.Chain): Response {
-            val originalRequest = chain.request()
-            // TODO datastore에 암호화되어 저장된 AccessToken 불러오기
-            val authRequest =
-                originalRequest.newBuilder().addHeader(ACCESS_TOKEN, "AccessToken").build()
-            val response = chain.proceed(authRequest)
+@Inject
+constructor(
+    private val json: Json,
+    private val userLocalRepository: UserLocalRepository,
+    private val context: Application
+) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originalRequest = chain.request()
 
-            when (response.code) {
-                401 -> {
-                    // TODO 토큰 재발급 api 연동
-                }
+        val accessToken =
+            runBlocking {
+                userLocalRepository.userPreferenceFlow
+                    .map { it.accessToken }
+                    .firstOrNull()
             }
-            return response
-        }
+        val isLogin =
+            runBlocking {
+                userLocalRepository.userPreferenceFlow
+                    .map { it.isLogin }
+                    .firstOrNull()
+            }
 
-        companion object {
-            const val ACCESS_TOKEN = "accessToken"
+        Timber.d("[인터셉터] -> AuthInterceptor에 $accessToken")
+        Timber.d("[인터셉터] -> AuthInterceptor에 $isLogin")
+
+        val authRequest = if (isLogin == true) originalRequest.newBuilder().addHeader(ACCESS_TOKEN, "$accessToken").build() else originalRequest
+        val response = chain.proceed(authRequest)
+
+        when (response.code) {
+            EXPIRE_TOKEN_CODE -> {
+                // TODO 토큰 재발급 api 연동
+            }
         }
+        return response
     }
+
+    companion object {
+        const val ACCESS_TOKEN = "Authorization"
+        const val EXPIRE_TOKEN_CODE = 401
+    }
+}
--- app/src/main/java/org/android/bbangzip/di/UseCaseModule.kt
+++ app/src/main/java/org/android/bbangzip/di/UseCaseModule.kt
@@ -5,7 +5,9 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import org.android.bbangzip.domain.repository.remote.DummyRepository
+import org.android.bbangzip.domain.repository.remote.UserRepository
 import org.android.bbangzip.domain.usecase.FetchDummyUseCase
+import org.android.bbangzip.domain.usecase.PostOnboardingUseCase
 import javax.inject.Singleton
 
 @Module
@@ -15,4 +17,9 @@ class UseCaseModule {
     @Singleton
     fun providesDummyFetchUseCase(dummyRepository: DummyRepository): FetchDummyUseCase =
         FetchDummyUseCase(dummyRepository = dummyRepository)
+
+    @Provides
+    @Singleton
+    fun providesPostOnboardingUseCase(userRepository: UserRepository): PostOnboardingUseCase =
+        PostOnboardingUseCase(userRepository = userRepository)
 }
