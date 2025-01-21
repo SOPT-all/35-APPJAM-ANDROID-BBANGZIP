2e21037c51aff72d306567f503e859f26738b931
JIEUNI <jieundaeun@naver.com>
2025-01-21 16:30:52 +0900

    [add] #49 ApiConstants 추가

--- app/src/main/java/org/android/bbangzip/data/util/constant/ApiConstants.kt
+++ app/src/main/java/org/android/bbangzip/data/util/constant/ApiConstants.kt
@@ -1,6 +1,17 @@
 package org.android.bbangzip.data.util.constant
 
 object ApiConstants {
+    const val API = "api"
     const val VERSION = "v1"
     const val DUMMY = "dummy"
+
+    // User
+    const val USER = "user"
+    const val AUTH = "auth"
+    const val SIGN_IN = "signin"
+    const val REISSUE = "re-issue"
+    const val SIGN_OUT = "signinout"
+    const val WITHDRAW = "withdraw"
+    const val SIGN_UP = "signup"
+    const val CODE = "code"
 }
