bf47145f3d4da4d85c4a0d742999da07a98c80fe
JIEUNI <jieundaeun@naver.com>
2025-01-24 21:22:59 +0900

    [chore] #72 BbangZipGetBadgeBottomSheet clip 처리

--- app/src/main/java/org/android/bbangzip/presentation/component/bottomsheet/BbangZipGetBadgeBottomSheet.kt
+++ app/src/main/java/org/android/bbangzip/presentation/component/bottomsheet/BbangZipGetBadgeBottomSheet.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
@@ -25,6 +26,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -220,7 +222,9 @@ private fun BadgeDetailContent(
                         .crossfade(enable = true)
                         .build(),
                 contentDescription = null,
-                modifier = Modifier.align(Alignment.CenterHorizontally),
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(48.dp))
+                    .align(Alignment.CenterHorizontally),
             )
 
             Spacer(modifier = Modifier.height(8.dp))
