d74ba2492e5513eea212348da2e8f552e175c226
kamja0510 <kamja990510@gmail.com>
2025-02-18 02:46:33 +0900

    [chore] #81 textfield 제한 글자수 상수화

--- app/src/main/java/org/android/bbangzip/presentation/ui/subject/addsubject/AddSubjectScreen.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/subject/addsubject/AddSubjectScreen.kt
@@ -27,6 +27,8 @@ import org.android.bbangzip.presentation.ui.subject.addstudy.AddStudyContract
 import org.android.bbangzip.presentation.util.modifier.addFocusCleaner
 import org.android.bbangzip.ui.theme.BbangZipTheme
 
+private const val TEXT_FIELD_MAX_CHARACTER = 10
+
 @Composable
 fun AddSubjectScreen(
     state: AddSubjectContract.AddSubjectState,
@@ -75,7 +77,7 @@ fun AddSubjectScreen(
                 onValueChange = onSubjectNameChange,
                 onFocusChange = onTextFieldFocusChange,
                 onDeleteButtonClick = onDeleteBtnClick,
-                maxCharacter = 10,
+                maxCharacter = TEXT_FIELD_MAX_CHARACTER,
                 focusManager = focusManager,
             )
 
