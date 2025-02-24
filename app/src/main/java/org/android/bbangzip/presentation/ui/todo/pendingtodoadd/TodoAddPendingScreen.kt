100d4d469966fc311ba2614a5578f47afaa40cd7
beom84 <cjja00@konkuk.ac.kr>
2025-02-24 15:57:22 +0900

    [refactor] statusbar 선언 위치 변경

--- app/src/main/java/org/android/bbangzip/presentation/ui/todo/pendingtodoadd/TodoAddPendingScreen.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/todo/pendingtodoadd/TodoAddPendingScreen.kt
@@ -1,5 +1,6 @@
 package org.android.bbangzip.presentation.ui.todo.pendingtodoadd
 
+import android.app.Activity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -24,7 +25,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -53,6 +56,8 @@ fun TodoAddPendingScreen(
     onItemPlusBtnClick: () -> Unit = {},
     onToDoCardClick: (Int, BbangZipCardState) -> Unit = { _, _ -> },
 ) {
+    (LocalView.current.context as Activity).window.statusBarColor = BbangZipTheme.colors.backgroundNormal_FFFFFF.toArgb()
+
     Box(
         modifier =
             modifier
--- app/src/main/java/org/android/bbangzip/presentation/ui/todo/todoadd/TodoAddRoute.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/todo/todoadd/TodoAddRoute.kt
@@ -1,26 +1,17 @@
 package org.android.bbangzip.presentation.ui.todo.todoadd
 
-import android.app.Activity
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import org.android.bbangzip.ui.theme.BbangZipTheme
+import org.android.bbangzip.presentation.component.indicator.BbangZipLoadingIndicator
 
 @Composable
 fun TodoAddRoute(
@@ -32,11 +23,7 @@ fun TodoAddRoute(
     val todoAddState by viewModel.uiState.collectAsStateWithLifecycle()
     val success by viewModel.success.collectAsStateWithLifecycle(initialValue = false)
     val todoAddsSnackBarHostState = remember { SnackbarHostState() }
-    val view = LocalView.current
     val context = LocalContext.current
-    val activity = view.context as Activity
-
-    activity.window.statusBarColor = BbangZipTheme.colors.staticWhite_FFFFFF.toArgb()
 
     LaunchedEffect(viewModel.uiSideEffect) {
         viewModel.uiSideEffect.collectLatest { effect ->
@@ -96,14 +83,6 @@ fun TodoAddRoute(
             )
 
         false ->
-            Box(
-                modifier =
-                    Modifier
-                        .fillMaxSize()
-                        .background(color = BbangZipTheme.colors.staticWhite_FFFFFF),
-                contentAlignment = Alignment.Center,
-            ) {
-                CircularProgressIndicator(color = BbangZipTheme.colors.backgroundAccent_FFDAA0)
-            }
+            BbangZipLoadingIndicator()
     }
 }
