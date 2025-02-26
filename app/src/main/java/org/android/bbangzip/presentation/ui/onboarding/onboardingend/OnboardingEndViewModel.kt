6c1d9d1c6d8322c957cc442b24158e333c0f95ce
JIEUNI <jieundaeun@naver.com>
2025-02-26 21:35:19 +0900

    [chore] #90 ktlint 적용

--- app/src/main/java/org/android/bbangzip/presentation/ui/onboarding/onboardingend/OnboardingEndViewModel.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/onboarding/onboardingend/OnboardingEndViewModel.kt
@@ -19,63 +19,63 @@ import javax.inject.Inject
 
 @HiltViewModel
 class OnboardingEndViewModel
-@Inject
-constructor(
-    private val userLocalRepository: UserLocalRepository,
-    private val postOnboardingUseCase: PostOnboardingUseCase,
-    savedStateHandle: SavedStateHandle,
-) : BaseViewModel<OnboardingEndContract.OnboardingEndEvent, OnboardingEndContract.OnboardingEndState, OnboardingEndContract.OnboardingEndReduce, OnboardingEndContract.OnboardingEndSideEffect>(
-    savedStateHandle = savedStateHandle,
-) {
-    private val userPreferencesFlow: Flow<UserPreferences> = userLocalRepository.userPreferenceFlow
+    @Inject
+    constructor(
+        private val userLocalRepository: UserLocalRepository,
+        private val postOnboardingUseCase: PostOnboardingUseCase,
+        savedStateHandle: SavedStateHandle,
+    ) : BaseViewModel<OnboardingEndContract.OnboardingEndEvent, OnboardingEndContract.OnboardingEndState, OnboardingEndContract.OnboardingEndReduce, OnboardingEndContract.OnboardingEndSideEffect>(
+            savedStateHandle = savedStateHandle,
+        ) {
+        private val userPreferencesFlow: Flow<UserPreferences> = userLocalRepository.userPreferenceFlow
 
-    override fun createInitialState(savedState: Parcelable?): OnboardingEndContract.OnboardingEndState {
-        return savedState as? OnboardingEndContract.OnboardingEndState
-            ?: OnboardingEndContract.OnboardingEndState()
-    }
+        override fun createInitialState(savedState: Parcelable?): OnboardingEndContract.OnboardingEndState {
+            return savedState as? OnboardingEndContract.OnboardingEndState
+                ?: OnboardingEndContract.OnboardingEndState()
+        }
 
-    override fun handleEvent(event: OnboardingEndContract.OnboardingEndEvent) {
-        when (event) {
-            is OnboardingEndContract.OnboardingEndEvent.OnBackBtnClick -> setSideEffect(OnboardingEndContract.OnboardingEndSideEffect.NavigateToBack)
-            is OnboardingEndContract.OnboardingEndEvent.OnCompleteBtnClick -> {
-                val onboardingInfo = runBlocking { userPreferencesFlow.map { it.onboardingInfo }.firstOrNull() }
-                Timber.d("[온보딩] -> $onboardingInfo")
-                postOnboardingInfo(onboardingInfo = onboardingInfo)
+        override fun handleEvent(event: OnboardingEndContract.OnboardingEndEvent) {
+            when (event) {
+                is OnboardingEndContract.OnboardingEndEvent.OnBackBtnClick -> setSideEffect(OnboardingEndContract.OnboardingEndSideEffect.NavigateToBack)
+                is OnboardingEndContract.OnboardingEndEvent.OnCompleteBtnClick -> {
+                    val onboardingInfo = runBlocking { userPreferencesFlow.map { it.onboardingInfo }.firstOrNull() }
+                    Timber.d("[온보딩] -> $onboardingInfo")
+                    postOnboardingInfo(onboardingInfo = onboardingInfo)
+                }
+            }
+        }
+
+        override fun reduceState(
+            state: OnboardingEndContract.OnboardingEndState,
+            reduce: OnboardingEndContract.OnboardingEndReduce,
+        ): OnboardingEndContract.OnboardingEndState {
+            return when (reduce) {
+                is OnboardingEndContract.OnboardingEndReduce.UpdateState -> reduce.state
+            }
+        }
+
+        private fun postOnboardingInfo(onboardingInfo: org.android.bbangzip.OnboardingInfo?) {
+            viewModelScope.launch {
+                Timber.d("[온보딩] 온보딩 POST -> ${onboardingInfo?.userName}, ${onboardingInfo?.year}, ${onboardingInfo?.semester}, ${onboardingInfo?.subjectName} ")
+                postOnboardingUseCase.invoke(
+                    OnboardingEntity(
+                        nickname = onboardingInfo?.userName ?: "",
+                        year = onboardingInfo?.year ?: 0,
+                        semester = onboardingInfo?.semester ?: "",
+                        subjectName = onboardingInfo?.subjectName ?: "",
+                    ),
+                ).onSuccess {
+                    saveIsOnboardingCompleted(isOnboardingCompleted = true)
+                    setSideEffect(OnboardingEndContract.OnboardingEndSideEffect.NavigateToSubject)
+                }.onFailure {
+                    Timber.d("[온보딩] 서버통신 실패 -> $error")
+                }
+            }
+        }
+
+        private fun saveIsOnboardingCompleted(isOnboardingCompleted: Boolean) {
+            viewModelScope.launch {
+                userLocalRepository.setIsOnboardingCompleted(isOnboardingCompleted = isOnboardingCompleted)
             }
         }
     }
-
-    override fun reduceState(
-        state: OnboardingEndContract.OnboardingEndState,
-        reduce: OnboardingEndContract.OnboardingEndReduce,
-    ): OnboardingEndContract.OnboardingEndState {
-        return when (reduce) {
-            is OnboardingEndContract.OnboardingEndReduce.UpdateState -> reduce.state
-        }
-    }
-
-    private fun postOnboardingInfo(onboardingInfo: org.android.bbangzip.OnboardingInfo?) {
-        viewModelScope.launch {
-            Timber.d("[온보딩] 온보딩 POST -> ${onboardingInfo?.userName}, ${onboardingInfo?.year}, ${onboardingInfo?.semester}, ${onboardingInfo?.subjectName} ")
-            postOnboardingUseCase.invoke(
-                OnboardingEntity(
-                    nickname = onboardingInfo?.userName ?: "",
-                    year = onboardingInfo?.year ?: 0,
-                    semester = onboardingInfo?.semester ?: "",
-                    subjectName = onboardingInfo?.subjectName ?: "",
-                ),
-            ).onSuccess {
-                saveIsOnboardingCompleted(isOnboardingCompleted = true)
-                setSideEffect(OnboardingEndContract.OnboardingEndSideEffect.NavigateToSubject)
-            }.onFailure {
-                Timber.d("[온보딩] 서버통신 실패 -> $error")
-            }
-        }
-    }
-
-    private fun saveIsOnboardingCompleted(isOnboardingCompleted: Boolean) {
-        viewModelScope.launch {
-            userLocalRepository.setIsOnboardingCompleted(isOnboardingCompleted = isOnboardingCompleted)
-        }
-    }
-}
