a78ea47eeee9bb0e670838ca590ccc1fc9598729
JIEUNI <jieundaeun@naver.com>
2025-02-26 20:59:00 +0900

    [chore] #90 전체적으로 불필요한 import 제거 및 코드 정렬

--- app/src/main/java/org/android/bbangzip/presentation/ui/onboarding/onboardingstart/OnboardingStartViewModel.kt
+++ app/src/main/java/org/android/bbangzip/presentation/ui/onboarding/onboardingstart/OnboardingStartViewModel.kt
@@ -8,29 +8,29 @@ import javax.inject.Inject
 
 @HiltViewModel
 class OnboardingStartViewModel
-    @Inject
-    constructor(
-        savedStateHandle: SavedStateHandle,
-    ) : BaseViewModel<OnboardingStartContract.OnboardingStartEvent, OnboardingStartContract.OnboardingStartState, OnboardingStartContract.OnboardingStartReduce, OnboardingStartContract.OnboardingStartSideEffect>(
-            savedStateHandle = savedStateHandle,
-        ) {
-        override fun createInitialState(savedState: Parcelable?): OnboardingStartContract.OnboardingStartState {
-            return savedState as? OnboardingStartContract.OnboardingStartState
-                ?: OnboardingStartContract.OnboardingStartState()
-        }
+@Inject
+constructor(
+    savedStateHandle: SavedStateHandle,
+) : BaseViewModel<OnboardingStartContract.OnboardingStartEvent, OnboardingStartContract.OnboardingStartState, OnboardingStartContract.OnboardingStartReduce, OnboardingStartContract.OnboardingStartSideEffect>(
+    savedStateHandle = savedStateHandle,
+) {
+    override fun createInitialState(savedState: Parcelable?): OnboardingStartContract.OnboardingStartState {
+        return savedState as? OnboardingStartContract.OnboardingStartState
+            ?: OnboardingStartContract.OnboardingStartState()
+    }
 
-        override fun handleEvent(event: OnboardingStartContract.OnboardingStartEvent) {
-            when (event) {
-                is OnboardingStartContract.OnboardingStartEvent.OnNextBtnClick -> setSideEffect(OnboardingStartContract.OnboardingStartSideEffect.NavigateToOnboarding)
-            }
-        }
-
-        override fun reduceState(
-            state: OnboardingStartContract.OnboardingStartState,
-            reduce: OnboardingStartContract.OnboardingStartReduce,
-        ): OnboardingStartContract.OnboardingStartState {
-            return when (reduce) {
-                is OnboardingStartContract.OnboardingStartReduce.UpdateState -> reduce.state
-            }
+    override fun handleEvent(event: OnboardingStartContract.OnboardingStartEvent) {
+        when (event) {
+            is OnboardingStartContract.OnboardingStartEvent.OnNextBtnClick -> setSideEffect(OnboardingStartContract.OnboardingStartSideEffect.NavigateToOnboarding)
         }
     }
+
+    override fun reduceState(
+        state: OnboardingStartContract.OnboardingStartState,
+        reduce: OnboardingStartContract.OnboardingStartReduce,
+    ): OnboardingStartContract.OnboardingStartState {
+        return when (reduce) {
+            is OnboardingStartContract.OnboardingStartReduce.UpdateState -> reduce.state
+        }
+    }
+}
