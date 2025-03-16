94283dd8e19d5ae95e3aae448ef4d87daa392f50
beom84 <cjja00@konkuk.ac.kr>
2025-03-17 01:12:55 +0900

    [feat] benchmark 오류 -> 경고로 수정

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -11,18 +11,18 @@ android {
     compileSdk = 35
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 
     defaultConfig {
         minSdk = 28
         targetSdk = 35
-
+        testInstrumentationRunnerArguments["androidx.benchmark.suppressErrors"] = "EMULATOR"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
@@ -53,7 +53,6 @@ dependencies {
     implementation(libs.androidx.uiautomator)
     implementation(libs.androidx.benchmark.macro.junit4)
     implementation(libs.androidx.profileinstaller)
-    implementation(project(":baselineprofile"))
 }
 
 androidComponents {
