b4b350cf1e9e8d4b7b947ca2c26a967163a8249a
beom84 <cjja00@konkuk.ac.kr>
2025-03-17 01:53:39 +0900

    [feat] baseline profile emulator 수정

--- baselineprofile/build.gradle.kts
+++ baselineprofile/build.gradle.kts
@@ -32,8 +32,8 @@ android {
     // To use GMD please invoke generation through the command line:
     // ./gradlew :app:generateBaselineProfile
     testOptions.managedDevices.devices {
-        create<ManagedVirtualDevice>("pixel6Api34") {
-            device = "Pixel 6"
+        create<ManagedVirtualDevice>("pixel3aApi34") {
+            device = "Pixel 3a"
             apiLevel = 34
             systemImageSource = "google"
         }
@@ -43,8 +43,8 @@ android {
 // This is the configuration block for the Baseline Profile plugin.
 // You can specify to run the generators on a managed devices or connected devices.
 baselineProfile {
-    managedDevices += "pixel6Api34"
-    useConnectedDevices = false
+    managedDevices += "pixel3aApi34"
+    useConnectedDevices = true
 }
 
 dependencies {
